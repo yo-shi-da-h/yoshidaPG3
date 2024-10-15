f71052c291c6d33d12af3e6eaeba76856043b913
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2024-10-15 15:47:44 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,65 @@
 #include <stdio.h>
 #include <Windows.h>
-int main() {
-	SetConsoleOutputCP(65001);
+#include <stdlib.h>
+#include <time.h>
 
-	printf("ああああ");
+typedef void (*PFunc)(int *);
+
+void DispResult(int* s) {
+	printf("%d秒待って実行されたよ\n", *s);
+}
+
+void setTimeout(PFunc p, int second) {
+
+	Sleep(second * 1000);
+
+	p(&second);
+}
+
+int main() {
+	
+	
+
+	 int userChoice; 
+    int dice;
+
+    
+    srand(time(0)); 
+
+    dice = rand() % 6 + 1;  
+
+    
+    printf("サイコロの目を奇数か偶数かを当てる。\n");
+    printf("1: 奇数, 2: 偶数\n");
+    printf("選択肢を入力 (1または2): ");
+    scanf_s("%d", &userChoice);
+
+     printf("start\n");
+
+	PFunc p;
+	p = DispResult;
+	setTimeout( p,3);
+
+    
+    printf("サイコロの目は: %d\n", dice);
+
+   
+    if (dice % 2 == 0) {
+        
+        if (userChoice == 2) {
+            printf("正解\n");
+        } else {
+            printf("不正解\n");
+        }
+    } else {
+        
+        if (userChoice == 1) {
+            printf("正解\n");
+        } else {
+            printf("不正解\n");
+        }
+    }
+   
 
 	return 0;
 }
