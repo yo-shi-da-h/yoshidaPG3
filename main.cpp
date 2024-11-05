612b69f13badb299f89c4609eefeae14ce22c500
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2024-11-05 16:21:23 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <stdio.h>
-#include <Windows.h>
+#include <iostream>
+#include <type_traits>
+
+template <typename T, typename U>
+class Minlator
+{
+
+public:
+    T min(T a, U b) {
+        return (a < b) ? a : b;
+    }
+
+};
+
 int main() {
-	SetConsoleOutputCP(65001);
 
-	printf("ああああ");
 
-	return 0;
+    Minlator<int, int> intInt;
+    Minlator<int, float> intFloat;
+    Minlator<int, double> intDouble;
+    Minlator<float, float> floatFloat;
+    Minlator<float, double> floatDouble;
+    Minlator<double, double> doubleDouble;
+
+    printf("int, int: %d\n", intInt.min(3, 5));
+    printf("int, float: %.1f\n", intFloat.min(3, 5.5f));
+    printf("int, double: %.1f\n", intDouble.min(3, 5.5));
+    printf("float, float: %.1f\n", floatFloat.min(3.3f, 5.5f));
+    printf("float, double: %.1f\n", floatDouble.min(3.3f, 5.5));
+    printf("double, double: %.1f\n", doubleDouble.min(3.3, 5.5));
+
+    return 0;
 }
