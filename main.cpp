79f890ae349e4a08ddb9f534239a899f28010d04
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2025-02-07 20:01:15 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
-#include <stdio.h>
-#include <Windows.h>
-int main() {
-	SetConsoleOutputCP(65001);
+#include "stdio.h"
+#include <thread>
+#include <mutex>
+#include <cstdio>
 
-	printf("ああああ");
+void print_message(const char* message) {
+
+	printf("%s\n", message);
+}
+
+int main() {
+
+	
+	std::thread t1(print_message, "thread1");
+	t1.join();
+	std::thread t2(print_message, "thread2");
+	t2.join();
+	std::thread t3(print_message, "thread3");
+	t3.join();
 
 	return 0;
+
+
 }
