a1e1c4cf8727dae7a37878c6ac9aa4443494c879
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2025-02-07 20:36:07 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
-#include <stdio.h>
-#include <Windows.h>
+
+#include <cstdio>
+#include <string>
+#include <chrono>
+
+void CopyTime(const std::string& src) {
+    auto start = std::chrono::high_resolution_clock::now(); 
+    std::string dest = src; 
+    // コピー
+    auto end = std::chrono::high_resolution_clock::now(); 
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); 
+    printf("コピー: %lld ns\n", duration); 
+}
+
+void MoveTime(std::string&& src) {
+    auto start = std::chrono::high_resolution_clock::now(); 
+    std::string dest = std::move(src); 
+    // 移動 
+    auto end = std::chrono::high_resolution_clock::now(); 
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); 
+    printf("移動: %lld ns\n", duration); 
+}
+
 int main() {
-	SetConsoleOutputCP(65001);
+    std::string a(10000, 'a'); // 10,000文字の文字列を作成
 
-	printf("ああああ");
+    printf("100,000文字を移動とコピーで比較\n");
+    CopyTime(a);       // コピー時間を計測
+    MoveTime(std::move(a)); // 移動時間を計測
 
-	return 0;
+    return 0;
 }
